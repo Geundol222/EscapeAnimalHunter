d95a0b750554472c365285570abca89521883df0
rpoiu0320 <129036265+rpoiu0320@users.noreply.github.com>
2023-08-31 20:13:48 +0900

    feat: Animal Data
    동물 기본 수치들 ScriptableObject 로 설정 가능하게 구현, 동물들 추가할려면AnimalData에서 AnimalName 추가 후 순서에 맞게 ScriptableObject 설정

---
+++ Assets/RHU/Scripts/Data/AnimalData.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AniamlData", menuName = "Data/Animal")]
+public class AnimalData : ScriptableObject
+{
+    public enum AnimalName { None, Bear, Moose };
+
+    [SerializeField] AnimalInfo[] animals;
+    public AnimalInfo[] Animals { get { return animals; } }
+
+    [Serializable]
+    public class AnimalInfo
+    {
+        public AnimalName name;
+        public string description;
+        public GameObject prefab;
+
+        public int maxHp;
+        public int walkSpeed;
+        public int runSpeed;
+        public int damage;
+    }
+}
