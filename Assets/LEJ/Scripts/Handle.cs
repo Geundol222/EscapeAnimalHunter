9435fe3ea5c4adf19722e946091c952cea313e2f
Eo jin <ddw4500@naver.com>
2023-08-31 21:04:18 +0900

    feat: if you move handle grip position, now handle is rotate

--- Assets/LEJ/Scripts/Handle.cs
+++ Assets/LEJ/Scripts/Handle.cs
@@ -1,47 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using static UnityEngine.ProBuilder.AutoUnwrapSettings;
 using static UnityEngine.Rendering.HableCurve;
 
 public class Handle : MonoBehaviour
 {
     [SerializeField] float rotateAmount;
-    [SerializeField] float controllerSensitivity;
     [SerializeField] float currentAngle;
+    [SerializeField] float handleRotateSpeed;
     [SerializeField] float handleResetSpeed;
 
-    [SerializeField] List<Vector3> arcPoints = new List<Vector3>();
-    [SerializeField] int areaPointsNum;
+    [SerializeField] List<Vector3> circlePositions = new List<Vector3>();
+    [SerializeField] List<Vector3> changePointPositions = new List<Vector3>();
+    List<GameObject> areas = new List<GameObject>();
+    [SerializeField] int changePointsNum = 12;
+    [SerializeField] float colliderRadius;
     [SerializeField] GameObject sphereObj;
-    [SerializeField] GameObject areas;
+    [SerializeField] GameObject areasParentObj;
+
+    int prevPoint;
+    int curPoint;
 
     DetectHandleGrab rightHandleGrab;
     DetectHandleGrab leftHandleGrab;
 
-    bool isHandleGripped;
+    [SerializeField] bool isHandleGripped;
     bool isHandleOrigin;
-    float maxAngle = 90f;
-    float minAngle = -90f;
-    IXRSelectInteractor interactor;
 
     public void Awake()
     {
         rightHandleGrab = transform.GetChild(0).GetComponent<DetectHandleGrab>(); //ObjForRightHand
         leftHandleGrab = transform.GetChild(1).GetComponent<DetectHandleGrab>(); //ObjForLeftHand
-    }
 
-    public void Start()
-    {
-        MakeArcWithLine();
-        areas.transform.localRotation = Quaternion.Euler(50f, -11f, -82f);
-        areas.transform.localPosition = new Vector3(0f, 0.1f, 0f);
+        SetChangePoints();
+        areasParentObj.transform.localRotation = Quaternion.Euler(50f, 0f, 0f);
+
+        for (int i = 0; i < changePointsNum; i++)
+        {
+            changePointPositions.Add(areas[i].transform.position);
+        }
+
+        prevPoint = -1;
+        curPoint = -1;
     }
 
     public void Update()
     {
-
         if (rightHandleGrab.isGripHandle == true || leftHandleGrab.isGripHandle == true)
             isHandleGripped = true;
         else
@@ -51,12 +59,24 @@ public class Handle : MonoBehaviour
             if (!isHandleOrigin)
                 ResetRotation();
         }
+
+        if (isHandleGripped)
+        {
+            if (curPoint != CheckCurrentChangePoint())
+            {
+                Debug.Log("Have to change angle");
+                ChangeHadleAngle();
+
+                prevPoint = curPoint;
+                curPoint = CheckCurrentChangePoint();
+            }
+        }
     }
 
     public void ResetRotation()
     {
         //핸들을 놓으면 원래 각도로 천천히 돌아가기 위해 Lerp 사용
-        currentAngle = Mathf.Lerp(currentAngle, 0f, handleResetSpeed);
+        transform.rotation = Quaternion.Euler(-43f, 90f, Mathf.Lerp(currentAngle, 0f, handleResetSpeed));
 
         if (currentAngle == 0f)
             isHandleOrigin = true;
@@ -64,38 +84,64 @@ public class Handle : MonoBehaviour
 
     public void ChangeHadleAngle()
     {
-        if (isHandleGripped)
-        {
-            
-        }
+        currentAngle = transform.rotation.z;
+
+        if (prevPoint < curPoint || prevPoint == 11 && curPoint == 0)
+            transform.rotation = Quaternion.Euler(-43f, 90f, Mathf.Lerp(currentAngle, currentAngle + rotateAmount, handleRotateSpeed));
+        if (prevPoint > curPoint || prevPoint == 0 && curPoint == 11)
+            transform.rotation = Quaternion.Euler(-43f, 90f, Mathf.Lerp(currentAngle, currentAngle - rotateAmount, handleRotateSpeed));
     }
 
 
-    public void SetArea()
-    {
-    }
-
-    public void MakeArcWithLine()
+    public void SetChangePoints()
     {
         float startAngle = 0;
         float arcLength = 360f;
         float radius = 0.2f;
 
-        for (int i = 0; i <= areaPointsNum; i++)
+        for (int i = 0; i <= changePointsNum; i++)
         {
             float x = Mathf.Sin(Mathf.Deg2Rad * startAngle) * radius;
             float z = Mathf.Cos(Mathf.Deg2Rad * startAngle) * radius;
-            arcPoints.Add(new Vector3(x, 0f, z));
+            circlePositions.Add(new Vector3(x, 0f, z));
 
-            startAngle += (arcLength / areaPointsNum);
+            startAngle += (arcLength / changePointsNum);
         }
 
-        for (int i = 0; i < areaPointsNum; i++)
+        for (int i = 0; i < changePointsNum; i++)
         {
             GameObject area = Instantiate(sphereObj);
-            area.transform.SetParent(areas.transform);
+            area.transform.SetParent(areasParentObj.transform);
             area.transform.position = transform.position;
-            area.transform.position += arcPoints[i];
+            area.transform.position += circlePositions[i];
+
+            areas.Add(area);
         }
+    }
+
+    public int CheckCurrentChangePoint()
+    {
+        for (int i = 0; i < changePointsNum; i++)
+        {
+            Collider[] colliders = Physics.OverlapSphere(changePointPositions[i], colliderRadius);
+
+            foreach (Collider collider in colliders)
+            {
+                if (collider.tag == "RightController" || collider.tag == "LeftController")
+                {
+                    Debug.Log(i);
+                    return i;
+                }
+                else
+                    continue;
+            }
+        }
+
+        return -1;
+    }
+
+    public void CheckIfCurPointChange()
+    {
+        
     }
 }
