8de2f94c384093fe734d6ae6dd9717416d3b2ad1
rpoiu0320 <129036265+rpoiu0320@users.noreply.github.com>
2023-09-04 19:18:49 +0900

    feat: Spawner
    무작위 위치에 Instantiate 하도록 구현

--- Assets/RHU/Scripts/Test/Spawner.cs
+++ Assets/RHU/Scripts/Test/Spawner.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     [Header("SpawnData")]
+    [SerializeField] GameObject spawnPoint;
     [SerializeField] Transform focusPoint;      // 플레이어의 시작 위치
     [SerializeField] AnimalData animalData;
     [SerializeField] AnimalSpawnList[] animalSpawnList;
@@ -13,8 +14,9 @@ public class Spawner : MonoBehaviour
     [Header("SpawnRange")]
     [SerializeField][Range(0, 100)] int maxRadius;            // 플레이어를 기준으로 도넛 모양의 범위에 생성하기위해 maxRadius에서 minRadius를 제외한 위치에 생성
     [SerializeField][Range(0, 100)] int minRadius;
-    private GameObject spawnPoint;
-    private Random random;
+
+    [Header("Etc")]
+    [SerializeField] LayerMask groundLayer;
 
     [Serializable]
     public class AnimalSpawnList
@@ -25,24 +27,30 @@ public class Spawner : MonoBehaviour
 
     private void Awake()
     {
-        spawnPoint = transform.GetChild(0).gameObject;
+
     }
 
     private void Start()
     {
-        foreach(AnimalSpawnList spawnList in animalSpawnList)   // 생성, TODO : PoolManager를 사용하게끔 변경, 랜덤값으로 각 동물을 생성할 개수를 지정해야 할 것 같음
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        foreach (AnimalSpawnList spawnList in animalSpawnList)   // 생성, TODO : PoolManager를 사용하게끔 변경, 랜덤값으로 각 동물을 생성할 개수를 지정해야 할 것 같음
         {
             for (int i = 0; i < spawnList.spawnCount; i++)
             {
+                RandomSpawnPoint();
                 GameObject prefab = FindAnimalPrefab(spawnList.animalName);
-
-                //Instantiate(prefab, RandomSpawnPoint());        // TODO : 위치 랜덤값 추가
-                Instantiate(prefab, focusPoint);
+                RaycastHit hit = GroundCheck();
+                spawnPoint.transform.position = new Vector3(spawnPoint.transform.position.x, hit.point.y, spawnPoint.transform.position.z);
+                GameManager.Resource.Instantiate(prefab, spawnPoint.transform.position, Quaternion.Euler(0, Random.Range(-180, 180), 0));
             }
         }
     }
 
-    private GameObject FindAnimalPrefab(AnimalData.AnimalName animalName)
+    private GameObject FindAnimalPrefab(AnimalData.AnimalName animalName)   // Data에서 해당 동물의 Prefab을 가져오는 함수
     {
         foreach (AnimalData.AnimalInfo animalInfo in animalData.Animals)    // TODO : 반복문 줄일 방법 생각해보기
         {
@@ -50,18 +58,55 @@ public class Spawner : MonoBehaviour
                 return animalInfo.prefab;
         }
 
+        Debug.Log("Not Found Animal Prefab");
         return null;
     }
 
-    private Transform RandomSpawnPoint()
+    private void RandomSpawnPoint()     // 스폰될 위치를 지정된 범위 내에서 랜덤하게 뽑아줌
     {
-        spawnPoint.transform.Translate(new Vector3(random.Next(minRadius, maxRadius), 20, random.Next(minRadius, maxRadius)));
+        spawnPoint.transform.position = focusPoint.transform.position;      // 기본 위치로 초기화
 
-        //if (Physics.Raycast(spawnPoint.transform.position, spawnPoint.transform.forward)) // 개발중
-        //{
+        int randomX = Random.Range(minRadius, maxRadius);
+        int randomZ = Random.Range(minRadius, maxRadius);
 
-        //}
+        int randomOperator = Random.Range(0, 2);        // +, - 를 랜덤하게 출력
 
-        return spawnPoint.transform;
+        switch (randomOperator)
+        {
+            case 0:
+                randomX = -randomX;
+                break;
+
+            default :
+                break;
+        }
+
+        randomOperator = Random.Range(0, 2);
+
+        switch (randomOperator)
+        {
+            case 0:
+                randomZ = -randomZ;
+                break;
+
+            default:
+                break;
+        }
+
+        spawnPoint.transform.position = new Vector3(randomX, 30 , randomZ);
+    }
+
+    private RaycastHit GroundCheck()    // Ray에 부딛힌 땅의 위치를 반환
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(spawnPoint.transform.position, Vector3.down, out hitInfo, 50, groundLayer))
+        {
+            return hitInfo;
+        }
+
+        hitInfo = new RaycastHit();
+        Debug.Log("Not Found Ground Layer");
+        return hitInfo;
     }
 }
