f7f8fef2527a48ca998ff0ac07f2018444997d73
Eo jin <ddw4500@naver.com>
2023-09-07 16:55:35 +0900

    feat: car has gravity

---
+++ Assets/LEJ/Scripts/Car/CarMover.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour
+{
+    [SerializeField] float sphereCastRadius;
+    [SerializeField] float castHeight;
+    [SerializeField] float maxDistant;
+
+    CharacterController controller;
+    float ySpeed;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Fall();
+    }
+
+    private void Fall()
+    {
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (GroundCheck() && ySpeed < 0)
+        {
+
+            ySpeed = 0;
+        }
+
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);
+    }
+
+    private bool GroundCheck()
+    {
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position + Vector3.up * castHeight, sphereCastRadius, Vector3.down, out hit, maxDistant);
+    }
+}
